f1700d0cbeaf03e80828579b6c58cd3930826386
aqueralto <albert.queralto.lopez@gmail.com>
2022-09-16 07:40:01 +0200

    added function

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,9 @@
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
+from locator import *
 
 class BasePage(object):
     
@@ -9,4 +13,17 @@ class BasePage(object):
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
         
-class NavigationFunctions(BasePage)
+class NavigationFunctions(BasePage):
+    
+    def open_chrome_session(self):
+        while True:
+            try:
+                self.drive.get(f"{self.base_url}")
+                print(f"{self.base_url}")
+                break
+            except TimeoutException as timeout:
+                print(f"{timeout}")
+                print("Page will be reloaded.")
+                
+    def navigate_to_lists_page(self):
+        link = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(MainPageLocators.MORE_BOOK_LISTS))
