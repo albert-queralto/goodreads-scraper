58787054d5079e34f84a16d353b9e862a2d31a41
aqueralto <albert.queralto.lopez@gmail.com>
2022-09-17 11:29:48 +0200

    added call to methods

--- src/__main__.py
+++ src/__main__.py
@@ -3,11 +3,25 @@ from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
-
+from navigation import *
 
 class BookScraping(unittest.TestCase):
     
-    def driver_config(self):
+    def setUp(self):
         self.driver = Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.driver.set_page_load_timeout(10)
         self.base_url = 'http://goodreads.com/'
+        
+    def test_scraper(self):
+        page_navigator = Navigation(driver=self.driver, base_url=self.base_url)
+        page_navigator.open_chrome_session()
+        page_navigator.navigate_to_lists_page()
+        page_navigator.click_x_button()
+        page_navigator.navigate_best_books_ever()
+        
+        
+    def tearDown(self):
+        self.driver.close()
+        
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
