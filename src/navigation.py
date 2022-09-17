07d1d98fc097ccb1fd933fe60c90da86cfcd7c53
aqueralto <albert.queralto.lopez@gmail.com>
2022-09-17 11:36:14 +0200

    added new functions to Navigation class

--- src/navigation.py
+++ src/navigation.py
@@ -1,7 +1,10 @@
 
 
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
-
+from font_code_pointers import *
 
 
 class BasePage(object):
@@ -18,3 +21,19 @@ class Navigation(BasePage):
             try:
                 self.driver.get(f"{self.base_url}")
                 print(f"{self.base_url}")
+                break
+            except TimeoutException as timeout:
+                print(f"{timeout}")
+                print(f"Page will be reloaded.")
+                
+    def navigate_to_lists_page(self):
+        link = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(MainPagePointers.MORE_BOOK_LISTS))
+        self.driver.execute_script("arguments[0].click();", link)
+        
+    def click_x_button(self):
+        button = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(ListopiaPointers.X_BUTTON))
+        self.driver.execute_script("arguments[0].click();", button)
+        
+    def navigate_best_books_ever(self):
+        link = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(ListopiaPointers.BEST_BOOKS_EVER))
+        self.driver.execute_script("arguments[0].click();", link)
