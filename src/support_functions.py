830c11b26166cd0438487bdb0d67d0ad1a5341d7
aqueralto <albert.queralto.lopez@gmail.com>
2022-09-17 13:19:45 +0200

    added get_scores and get_votes functions

--- src/support_functions.py
+++ src/support_functions.py
@@ -7,3 +7,9 @@ class StringProcessing(object):
         stars = float(re.findall(r'\d+\.\d+', stripped_text[0])[0])
         ratings = int(stripped_text[1].split(' rating')[0].replace(',', ''))
         return stars, ratings
+    
+    def get_scores(text):
+        return int(text.split('score: ')[1].split('</a>')[0].replace(',', ''))
+    
+    def get_votes(text):
+        return int(text.split(' people voted')[0].split('>')[1].replace(',', ''))
